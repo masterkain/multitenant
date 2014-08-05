a03fa23e5708b7c4e3264d3e625b80994bea8d89
Claudio Poli <masterkain@gmail.com>
2014-08-05 02:26:09 +0200

    rails 4.1 compat, cannot return from callbacks this way, otherwise code will thrown a LocalJumpError
    remove rvmrc

--- lib/multitenant.rb
+++ lib/multitenant.rb
@@ -20,13 +20,18 @@ module Multitenant
     # see Multitenant#current_tenant
     def belongs_to_multitenant(association = :tenant)
       reflection = reflect_on_association association
-      before_validation Proc.new {|m|
-        return unless Multitenant.current_tenant
-        m.send "#{association}=".to_sym, Multitenant.current_tenant
-      }, :on => :create
+      before_validation :set_association, on: :create
+
       default_scope lambda {
-        where({reflection.foreign_key => Multitenant.current_tenant.id}) if Multitenant.current_tenant
+        where(reflection.foreign_key => Multitenant.current_tenant.id) if Multitenant.current_tenant
       }
+
+      private
+
+      def set_association
+        return false unless Multitenant.current_tenant
+        m.send "#{association}=".to_sym, Multitenant.current_tenant
+      end
     end
   end
 end
